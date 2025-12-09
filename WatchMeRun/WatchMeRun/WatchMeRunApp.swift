705853c740a04f622f55a009a50e11927635c7cf
ckorabik <chris.korabik@gmail.com>
2025-12-09 17:17:53 -0600

    default XCode setup

---
+++ WatchMeRun/WatchMeRun/WatchMeRunApp.swift
@@ -0,0 +1,32 @@
+//
+//  WatchMeRunApp.swift
+//  WatchMeRun
+//
+//  Created by Christopher Korabik on 12/9/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WatchMeRunApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
